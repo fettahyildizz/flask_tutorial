19806c1cff03d112fedd3bed46bd5a6c276751b1
fettahyildiz <fettah.yildiz@delivers.ai>
2023-02-23 18:31:58 +0300

    added layout template

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Fettah Yildiz',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'Feburary 23, 2023'
+    },
+    {
+        'author': 'Fatih Baygul',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Feburary 25, 2023'
+    }
+]
 # Routes are what we type into browsers to go to different pages
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html', posts=posts, title='Main')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page!</h1>"
+    return render_template('about.html', posts=posts, title='About')
 
 if __name__ == '__main__':
     app.run(debug=True )
